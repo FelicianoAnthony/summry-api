b5409ae6ac33b010b28ca483c5ce646b3ec11b5c
unknown <afeliciano893@gmail.com>
2023-03-28 22:58:53 -0400

    require both at least 1 store and query for a summry to exist

--- ApiModels/UserSummry/UserSummryPut.cs
+++ ApiModels/UserSummry/UserSummryPut.cs
@@ -1,12 +1,15 @@
 ﻿using StarterApi.ApiModels.Store;
 using StarterApi.ApiModels.UserSummryQuery;
+using System.ComponentModel.DataAnnotations;
 
 namespace StarterApi.ApiModels.UserSummry
 {
     public class UserSummryPut
     {
+        [MinLength(1)]
         public List<StorePost> Stores { get; set; }
 
+        [MinLength(1)]
         public List<UserSummryQueryPost> Queries { get; set; }
     }
 }
